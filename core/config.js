4a6c2eed310d8d1eb5260a78b04016c365a78009
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-01-20 20:54:29 +0100

    taking the pain away

--- core/config.js
+++ core/config.js
@@ -4,7 +4,7 @@ const jwtSecret = process.env.JWT_SECRET
 const served = process.env.NODE_ENV == 'serve'
 const GclientID = process.env.GclientID, GclientSecret = process.env.GclientSecret;
 const FclientID = process.env.FclientID, FclientSecret = process.env.FclientSecret;
-const siteAddress = "http://localhost:3000";
+const siteAddress = process.envv.BASE_URL || "http://localhost:3000";
 
 module.exports = {
     mongoUrl,
